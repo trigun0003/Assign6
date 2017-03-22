cf4c0cb36d63ecdd925ab3f0c96ec432829450b4
trigun0003 <hebert54@hotmail.com>
2017-03-22 15:53:05 -0400

    First Commit to create Brances

---
+++ src/java/rest/Message.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rest;
+
+/**
+ *
+ * @author c0687631
+ */
+public class Message {
+    
+}
