3e731e4ad910b7e507fa80b37596f57a9a8d7ffc
c0690651 <c0690651@SB-N208-17.lambton.on.ca>
2017-03-29 12:51:42 -0400

    messageController & database connection are finished...

--- src/java/rest/DBUtils.java
+++ src/java/rest/DBUtils.java
@@ -19,15 +19,25 @@ import javax.enterprise.context.ApplicationScoped;
 
 @ApplicationScoped
 public class DBUtils {
+   private final static String studentNumber = "c0690651";
+
+    /**
+     * Utility method used to create a Database Connection
+     *
+     * @return the Connection object
+     * @throws SQLException
+     */
     public static Connection getConnection() throws SQLException {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(DBUtils.class.getName()).log(Level.SEVERE, null, ex);
         }
-        String jdbc = "jdbc:mysql://ipro:3306/messagedb";
-        String user = "messagedb";
-        String pass = "password";
-        return DriverManager.getConnection(jdbc, user, pass);
+        String server = "ipro.lambton.on.ca";
+        String username = studentNumber + "-java";
+        String password = studentNumber;
+        String database = username;
+        String jdbc = String.format("jdbc:mysql://%s/%s", server, database);
+        return DriverManager.getConnection(jdbc, username, password);
     }
 }
